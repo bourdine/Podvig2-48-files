c6c9acef2891acd54a8d3b4ec393f7000171af5f
Andrei Bourdine <bourdine@gmail.com>
2026-03-13 01:12:18 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    kotlin("kapt")
+    // id("com.google.dagger.hilt.android") - временно отключаем
+    // kotlin("kapt") - временно отключаем
 }
 
 android {
@@ -16,14 +16,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         externalNativeBuild {
             cmake {
-                arguments.addAll(listOf(
-                    "-DANDROID_STL=c++_shared",
-                    "-DANDROID_TOOLCHAIN=clang"
-                ))
+                arguments.addAll(listOf("-DANDROID_STL=c++_shared"))
                 cppFlags.add("-O3")
                 cppFlags.add("-fomit-frame-pointer")
                 cppFlags.add("-funroll-loops")
@@ -65,18 +60,24 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
+    
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    
     implementation("androidx.navigation:navigation-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("com.google.dagger:hilt-android:2.52")
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    
+    // Hilt временно отключен
+    // implementation("com.google.dagger:hilt-android:2.52")
+    // kapt("com.google.dagger:hilt-android-compiler:2.52")
+    // implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
@@ -88,10 +89,12 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 
-kapt {
-    correctErrorTypes = true
-}
+// kapt временно отключен
+// kapt {
+//     correctErrorTypes = true
+// }
