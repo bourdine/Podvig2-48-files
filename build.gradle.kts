3734ca04bf55e5676b478506e17642177f9ee5e3
Andrei Bourdine <bourdine@gmail.com>
2026-03-12 23:20:09 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-// опа
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    // опа
 }
 
 tasks.register("clean", Delete::class) {
