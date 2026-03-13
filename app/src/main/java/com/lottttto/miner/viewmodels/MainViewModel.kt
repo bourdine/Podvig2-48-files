238cbb87a5864f4d82671168c9702b17fd59bd5a
Andrei Bourdine <bourdine@gmail.com>
2026-03-13 12:34:04 +0300

    Update MainViewModel.kt

--- app/src/main/java/com/lottttto/miner/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/lottttto/miner/viewmodels/MainViewModel.kt
@@ -1,15 +1,12 @@
 package com.lottttto.miner.viewmodels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.lottttto.miner.models.CoinType
 import com.lottttto.miner.models.MiningMode
 import com.lottttto.miner.models.MiningTask
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     
