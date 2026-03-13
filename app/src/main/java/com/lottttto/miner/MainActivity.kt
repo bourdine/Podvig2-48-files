fa125e9d4993080acccd3070e3d2f2ee7ec70f68
Andrei Bourdine <bourdine@gmail.com>
2026-03-13 12:20:59 +0300

    Update MainActivity.kt

--- app/src/main/java/com/lottttto/miner/MainActivity.kt
+++ app/src/main/java/com/lottttto/miner/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.lottttto.miner.screens.MainScreen
+import com.lottttto.miner.screens.WalletScreen
 import com.lottttto.miner.ui.theme.LotttttoTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,26 +37,10 @@ fun AppNavigation() {
     
     NavHost(navController = navController, startDestination = "main") {
         composable("main") {
-            MainScreen()
+            MainScreen(navController = navController)
         }
         composable("wallet") {
-            WalletScreen()
+            WalletScreen(navController = navController)
         }
     }
-}
-
-@Composable
-fun MainScreen() {
-    Text(
-        text = "Lottttto Miner - Main Screen",
-        fontSize = 20.sp
-    )
-}
-
-@Composable
-fun WalletScreen() {
-    Text(
-        text = "Wallet Screen",
-        fontSize = 20.sp
-    )
 }
