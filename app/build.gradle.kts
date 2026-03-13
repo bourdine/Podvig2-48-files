19adc9232b7f63198531bb525c3598bd4be9b61b
Andrei Bourdine <bourdine@gmail.com>
2026-03-13 12:14:33 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,22 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
+    
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    
+    // Навигация
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+    
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
